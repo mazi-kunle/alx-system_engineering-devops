ad302e5213924f3d3e164b1f21f51deb2fb9884f
mazi-kunle <olakunlemichael644@gmail.com>
2022-10-15 11:10:59 -0700

    2

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# a manifest to create a file
+
+file {'/tmp/school':
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
