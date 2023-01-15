7bcb814c6434a31e9bd9388274eceea282723aa5
olakunle <olakunlemichael644@gmail.com>
2023-01-15 11:46:11 -0800

    7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,31 @@
+# A manifest to install and configure nginx
+
+# update apt
+exec {'update_apt':
+  command => 'sudo apt-get update',
+  path    => '/usr/bin'
+}
+
+# install nginx
+package {'install nginx':
+  ensure  => installed,
+  name    => 'nginx',
+  require => Exec['update_apt']
+}
+
+# create index.html
+file {'/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!'
+}
+
+exec {'redirect_me':
+  command => "sudo sed -i '25 i\\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}' /etc/nginx/sites-available/default",
+  path    => '/usr/bin'
+}
+
+# run nginx
+service {'nginx':
+  ensure  => running,
+  require => Package['install_nginx']
+}
