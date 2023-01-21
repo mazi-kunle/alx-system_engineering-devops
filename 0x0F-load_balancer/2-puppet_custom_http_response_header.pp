3be9765ac55053f09cebfe5c29f1ca4a07946dcd
mazi-kunle <olakunlemichael644@gmail.com>
2023-01-21 11:11:59 -0800

    final

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,38 @@
+# A manifest for creating a custom HTTP header
+
+# A manifest to install and configure nginx
+
+# update apt
+exec {'update_apt':
+  command => 'sudo apt-get update',
+  path    => '/usr/bin'
+}
+
+# install nginx
+package {'install_nginx':
+  ensure  => installed,
+  name    => 'nginx',
+  require => Exec['update_apt']
+}
+
+# create index.html
+file {'/var/www/html/index.html':
+  ensure  => present,
+  content => "Hello World!\n"
+}
+
+exec {'redirect_me':
+  command => 'sudo sed -i "25 i\	location /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}" /etc/nginx/sites-available/default',
+  path    => '/usr/bin'
+}
+
+exec {'add_header':
+  command => 'sudo sed -i "28 a\	add_header X-Served-By $hostname;" /etc/nginx/sites-available/default',
+  path    => '/usr/bin'
+}
+
+# run nginx
+service {'nginx':
+  ensure  => running,
+  require => Package['install_nginx']
+}
